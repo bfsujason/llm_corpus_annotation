c823847d8f7b9d20a5496bb6620f88ac0206a20b
bfsujason <bfsujason@163.com>
2026-02-25 16:06:35 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "正在安装项目所需模块..."
+pip install openai diskcache sentence_splitter hanlp pymusas
+
+echo "正在安装 spaCy 语言模型（离线安装）..."
+cd res
+for file in *.whl; do pip install "$file"; done
+cd ..
+
+echo "所有模块安装完成！"
