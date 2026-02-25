92ad039ded23880c4e77d8ef2a44ff51f78b499a
bfsujason <bfsujason@163.com>
2026-02-25 16:24:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "正在安装项目所需模块..."
 pip install openai diskcache sentence_splitter hanlp pymusas
 
 echo "正在安装 spaCy 语言模型（离线安装）..."
-cd res
+cd llm_corpus_annotation/res
 for file in *.whl; do pip install "$file"; done
 cd ..
 
